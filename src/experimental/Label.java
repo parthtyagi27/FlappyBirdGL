35e90d6fac6ca951db9062fd765126ed23d8a841
parthtyagi27 <parthtyagi27@users.noreply.github.com>
2019-04-12 19:03:17 -0500

    Improved label rendering algorithm

--- src/experimental/Label.java
+++ src/experimental/Label.java
@@ -4,11 +4,11 @@ import engine.Camera;
 import engine.Mesh;
 import engine.Renderer;
 import engine.Shader;
+import font.Text;
 import math.Transformation;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
-import javax.swing.event.TableColumnModelListener;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -17,12 +17,12 @@ public class Label
     private FontTexture fontTexture;
     private Map<Character, Glyph> map;
     private ArrayList<Mesh> meshList;
-    private float meshScale = 1;
+    private float meshScale;
     private float width = 0;
     private Matrix4f matrix;
+    private float padding;
     private String text;
 
-    public float x, y, z;
 
     public Label(FontTexture fontTexture, float scale)
     {
@@ -33,31 +33,48 @@ public class Label
         matrix = new Matrix4f();
     }
 
+    public Label(String text, FontTexture fontTexture, float scale)
+    {
+        this.fontTexture = fontTexture;
+        this.text = text;
+        map = fontTexture.getGlyphMap();
+        padding = fontTexture.getPadding();
+        meshList = new ArrayList<Mesh>();
+        meshScale = scale;
+        matrix = new Matrix4f();
+        //load the mesh of each character in text into meshList
+        loadText(text);
+    }
+
     public void loadText(String text)
     {
-        this.text = text;
         if(!meshList.isEmpty())
             meshList.clear();
         float startX = 0;
+        float o = 0;
         for(int i = 0; i < text.length(); i++)
         {
             Glyph glyph = map.get(text.charAt(i));
-            startX += glyph.getXoffset() - 5;
+            float xoffset = glyph.getXoffset();
+//            float yoffset = glyph.getYoffset();
+
             float[] verticies =
                     {
-                            (0 + startX) * meshScale, (glyph.getHeight())  * meshScale, 0.3f,
-                            (glyph.getWidth() + startX) * meshScale, (glyph.getHeight()) * meshScale, 0.3f,
-                            (glyph.getWidth() + startX) * meshScale, 0, 0.3f,
-                            (0 + startX) * meshScale, 0, 0.3f
+                            (0 + startX + xoffset) * meshScale, (glyph.getHeight())  * meshScale, 0.3f,
+                            (glyph.getWidth()  + startX + xoffset) * meshScale, (glyph.getHeight()) * meshScale, 0.3f,
+                            (glyph.getWidth()  + startX + xoffset) * meshScale, (0) * meshScale, 0.3f,
+                            (0 + startX + xoffset) * meshScale, (0) * meshScale, 0.3f
                     };
 
             float[] textures =
                     {
-                            (glyph.getX() / fontTexture.getTextureWidth()), (glyph.getY()) / fontTexture.getTextureHeight(),
-                            (glyph.getX() + glyph.getWidth()) / fontTexture.getTextureWidth(), (glyph.getY()) / fontTexture.getTextureHeight(),
-                            (glyph.getX() + glyph.getWidth()) / fontTexture.getTextureWidth(), (glyph.getY() + glyph.getHeight()) / fontTexture.getTextureHeight(),
-                            (glyph.getX() / fontTexture.getTextureWidth()), (glyph.getY() +  glyph.getHeight()) / fontTexture.getTextureHeight()
+                            ((glyph.getX() ) / fontTexture.getTextureWidth()), (glyph.getY()) / fontTexture.getTextureHeight(),
+                            (glyph.getX() + glyph.getWidth() ) / fontTexture.getTextureWidth(), (glyph.getY()) / fontTexture.getTextureHeight(),
+                            (glyph.getX() + glyph.getWidth() ) / fontTexture.getTextureWidth(), (glyph.getY() + glyph.getHeight()) / fontTexture.getTextureHeight(),
+                            ((glyph.getX() ) / fontTexture.getTextureWidth()), (glyph.getY() +  glyph.getHeight()) / fontTexture.getTextureHeight()
                     };
+//            System.out.println("Tex Coordinates");
+//            System.out.println((glyph.getX() / fontTexture.getTextureWidth()) + " " + ((glyph.getY()) / fontTexture.getTextureHeight()));
 
             meshList.add(new Mesh(verticies, textures));
             startX += glyph.getXadvance();
@@ -68,9 +85,6 @@ public class Label
     public void translate(float x, float y, float z)
     {
         matrix.translate(x, y, z);
-        this.x = x;
-        this.y = y;
-        this.z = z;
     }
 
     public void render(Shader shader, Camera camera)
@@ -82,11 +96,15 @@ public class Label
         shader.setUniform("projection", camera.getProjectionMatrix());
         shader.setUniform("model", matrix);
 
-        for(int i = 0; i < meshList.size(); i++)
-        {
-            Mesh m = meshList.get(i);
-            Renderer.drawMesh(m);
-        }
+        for(Mesh mesh : meshList)
+            Renderer.drawMesh(mesh);
+
+
         shader.unbind();
+    }
+
+    public float getWidth()
+    {
+        return width;
     }
 }
